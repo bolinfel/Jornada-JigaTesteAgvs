8dbbb9d6f11717279f0004a2d5f00c97b922d63d
Felipe Graf <62572082+bolinfel@users.noreply.github.com>
2024-10-10 21:40:20 -0300

    Criado o Models e Tabelas

--- JigaTesteAGVS/testes/models.py
+++ JigaTesteAGVS/testes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class tbPlacas(models.Model):
+    NOME = models.CharField(max_length=30)
+    COD = models.CharField(max_length=4)
+    ADD_CAN = models.CharField(max_length=30)
+
+class tbTipos(models.Model):
+    NOME = models.CharField(max_length=30)
+    COD = models.CharField(max_length=4)
+
+class tbPropriedades(models.Model):
+    FK_PLACA = models.ForeignKey(tbPlacas, on_delete=models.CASCADE)
+    FK_TIPO = models.ForeignKey(tbTipos, on_delete=models.CASCADE)
+    PORTA = models.IntegerField()
+    DESCRICAO = models.CharField(max_length=20)
+
+class tbTestes(models.Model):
+    DATA = models.TimeField(verbose_name="TIMESTAMP")
+    FK_PLACA = models.ForeignKey(tbPlacas, on_delete=models.CASCADE)
--- JigaTesteAGVS/testes/views.py
+++ JigaTesteAGVS/testes/views.py
@@ -1,41 +1,19 @@
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 from django.http import HttpResponse
 
 class TestesView(TemplateView):
    template_name = 'testes.html'
 
-class TesteAutomaticoView(TemplateView):
+class TesteAutomaticoView(CreateView):
   template_name = 'testeAutomatico.html'
   extra_context = {'Modelo':'CAN_IR',
                    'statusTeste':'fazer'
                    }
   
-  def teste_automatico_CAN(self):
-      #inserir script do teste automatico
-      self.extra_context['statusTeste'] = 'concluido'
-  
-  def get(self, request, *args, **kwargs):
-        
-    start_index = request.get_full_path_info().find('statusTeste=')
-
-    if start_index == -1:
-        status_value = 'fazer'
-    # Move the start index to the end of 'statusTeste='
-    start_index += len('statusTeste=')
-    status_value = request.get_full_path_info()[start_index:]
-    
-    if status_value == "iniciado":
-        self.teste_automatico_CAN()
-
-    return super().get(self.request, *args, **kwargs)
-
   
 
 class TesteManualView(TemplateView):
     template_name = 'testeManual.html'
-    extra_context = {'Modelo':'CAN_IR',
-                      'nEntradasDigitais':10,
-                      'nSaidasDigitais':10,
-                      'nEntradasAnalogicas':7,
-                      'nSaidasAnalogicas':9
-                    }
+    extra_context = {'Modelo':'CAN_IR'}
+    
