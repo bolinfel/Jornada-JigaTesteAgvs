6d7e6118f57a4408e1c3b073fdfe85078171bbaa
Kernel-Broch <lucasabroch@gmail.com>
2024-11-03 17:48:37 -0300

    Login

--- JigaTesteAGVS/home/views.py
+++ JigaTesteAGVS/home/views.py
@@ -1,4 +1,13 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
+
+class LogoutInterfaceView(LogoutView):
+  template_name='logout.html'
+
+
+class LoginInterfaceView(LoginView):
+  template_name='login.html'
+
 
 class HomeView(TemplateView):
   template_name = 'home.html'
