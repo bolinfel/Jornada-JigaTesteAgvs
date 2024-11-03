39e6cc8f5e2452be1af347a2db10b4f4403c6a8e
Bolinfel <bolinfel@gmail.com>
2024-11-03 17:20:11 -0300

    dalhes

--- JigaTesteAGVS/relatorios/urls.py
+++ JigaTesteAGVS/relatorios/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from relatorios.views import RelatoriosView, ListaView
+from relatorios.views import RelatoriosView, ListaView, DashboardView, DetalhesView
 
 urlpatterns = [
     path('', RelatoriosView.as_view(), name='relatorios'),
     path('lista', ListaView.as_view(), name='relatorios.lista'),
+    path('dashboard', DashboardView.as_view(), name='relatorios.dashboard'),
+    path('lista/<int:pk>/', DetalhesView.as_view(), name='relatorios.detalhes'),
 ]
--- JigaTesteAGVS/relatorios/views.py
+++ JigaTesteAGVS/relatorios/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from testes.models import tbTestes
 from time import timezone
 
@@ -14,3 +14,11 @@ class ListaView(ListView):
 
     # Opcional: Ordene os resultados, por exemplo, pela data do teste
     queryset = tbTestes.objects.all().order_by('-DATA')
+
+class DashboardView(TemplateView):
+    template_name = 'dashboard.html'
+
+class DetalhesView(DetailView):
+    model = tbTestes
+    template_name = 'detalhes.html'
+    context_object_name = 'teste'
